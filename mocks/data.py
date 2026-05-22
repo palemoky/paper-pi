50cc9819a94ea986b8e0ae5351a3e50a59d1b30c
Palemoky <palemoky@gmail.com>
2026-05-22 16:25:21 +0800

    fix: update weekly reset time in mock data for consistency

--- mocks/data.py
+++ mocks/data.py
@@ -27,7 +27,7 @@ def get_mock_dashboard_data() -> dict:
             "hourly_usage": 42,
             "weekly_usage": 66,
             "hourly_reset": "1h",
-            "weekly_reset": "2d5h",
+            "weekly_reset": "2d23h",
         },
         "todo_goals": ["Finish project ABCDEF", "Read a book", "Exercise"],
         "todo_must": ["Pay bills and reply to emails", "Reply to emails"],
